31f3d5fb425add0fac06f4db16538b64090090ef
aleksandranichols <aleksandra.nichols@gmail.com>
2019-09-22 12:37:48 -0500

    Rendering statistics cloud

--- js/stat.js
+++ js/stat.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var canvas = document.getElementById('statistics');
-var ctx = canvas.getContext('2d');
 var CLOUD_OFFSETX = 100;
 var CLOUD_OFFSETY = 10;
 var CLOUD_WIDTH = 420;
@@ -13,45 +11,40 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_MARGIN = 50;
 
-var renderCloud = function(ctx, shadowColor, cloudColor) {
+var renderCloud = function (ctx, shadowColor, cloudColor) {
   ctx.shadowColor = shadowColor;
   ctx.shadowOffsetX = SHADOW_OFFSETX;
   ctx.shadowOffsetY = SHADOW_OFFSETY;
-  // почему тень должна быть перед фигурой? я сперва написала параметры тени после fillStyle и в таком случае тень не появлялась.
   ctx.fillStyle = cloudColor;
   ctx.fillRect(CLOUD_OFFSETX, CLOUD_OFFSETY, CLOUD_WIDTH, CLOUD_HEIGHT);
-}
+};
 
-var getMaxNumber = function(arr) {
+var getMaxNumber = function (arr) {
+  var maxNumber = arr[0];
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i+1]) {
-      var maxNumber = arr[i];
+    if (arr[i] > maxNumber) {
+      maxNumber = arr[i];
     }
-    return maxNumber;
   }
-}
+  return maxNumber;
+};
 
-// maxNumber = 150;
-// times[i] = x
-
-window.renderStatistics = function(ctx, names, times) {
-  // почему вызов этой функции идет через window, а предыдущая объявляется через var? в чем вобще разнциа?
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', 'rgba(255, 255, 255, 1)');
-  ctx.shadowColor = 'transparent';
+  ctx.shadowColor = 'transparent'; // shadow causes a lot of pain
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.font = 'bold 16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_OFFSETX + TEXT_MARGIN, CLOUD_OFFSETY + TEXT_MARGIN);
-  ctx.fillText('Список результатов:', CLOUD_OFFSETX + TEXT_MARGIN, CLOUD_OFFSETY + TEXT_MARGIN*2);
+  ctx.fillText('Список результатов:', CLOUD_OFFSETX + TEXT_MARGIN, CLOUD_OFFSETY + TEXT_MARGIN * 2);
   for (var i = 0; i < names.length; i++) {
-    ctx.fillText(names[i], CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH+BAR_MARGIN) * i, CLOUD_OFFSETY + BAR_HEIGHT + TEXT_MARGIN * 4);
-    ctx.fillText((Math.floor(times[i])).toString(), CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH+BAR_MARGIN) * i, CLOUD_OFFSETY + TEXT_MARGIN * 3);
-    if (names[i] == 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)'
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillText(names[i], CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_OFFSETY + BAR_HEIGHT + TEXT_MARGIN * 4);
+    ctx.fillText(Math.round(times[i]), CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_OFFSETY + (TEXT_MARGIN * 3) + (BAR_HEIGHT - times[i] * BAR_HEIGHT / getMaxNumber(times)));
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(250, ' + Math.round(Math.random() * 101) + '%,' + Math.round(Math.random() * 101) + '%)';
     }
-    else {
-      var random = 'Math.floor(Math.random() * 101)' + '%';
-      ctx.fillStyle = 'hsl(250, random, random)';
-    }
-    ctx.fillRect(CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH+BAR_MARGIN) * i, CLOUD_OFFSETY + TEXT_MARGIN * 3, BAR_WIDTH, times[i] * BAR_HEIGHT / getMaxNumber(times));
+    ctx.fillRect(CLOUD_OFFSETX + BAR_MARGIN + (BAR_WIDTH + BAR_MARGIN) * i, CLOUD_OFFSETY + (TEXT_MARGIN * 3.5) + (BAR_HEIGHT - times[i] * BAR_HEIGHT / getMaxNumber(times)), BAR_WIDTH, times[i] * BAR_HEIGHT / getMaxNumber(times));
   }
-}
+};
